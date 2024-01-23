8e449ecedd832cd7cca4aeb1d603bfeedb46bb22
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-01-23 15:14:29 +0100

    commit

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -169,7 +169,7 @@ const Header = () => {
                   href="/kanteenAI"
                   className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-md bg-primary px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
                 >
-                  Leap AI
+                  Add Restaurant
                 </Link>
                 <div>
                   <ThemeToggler />
