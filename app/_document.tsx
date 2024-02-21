dfdd0737434468df8c63c963916af0fa4254656c
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-02-21 17:14:26 +0100

    commit

---
+++ app/_document.tsx
@@ -0,0 +1,35 @@
+// app/_document.js or app/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Other head elements */}
+
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-HPVVV7N9XM"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HPVVV7N9XM');
+            `}
+          </Script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
