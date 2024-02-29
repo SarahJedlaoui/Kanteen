b72111fe6db2526d84f986bcbfe404ed5e0e323b
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-02-29 12:54:01 +0100

    commit

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -42,8 +42,8 @@ const Header = () => {
     <>
       <header
         className={`header left-0 top-0 z-40 flex w-full items-center ${sticky
-            ? "dark:bg-gray-light dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
-            : "absolute bg-transparent"
+          ? "dark:bg-gray-light dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
+          : "absolute bg-transparent"
           }`}
       >
         <div className="container">
@@ -54,11 +54,29 @@ const Header = () => {
                 className={`header-logo block w-full ${sticky ? "py-5 lg:py-2" : "py-8"
                   }`}
               >
-                <span
+
+
+                <Image
+                  src="/images/logo/light.svg"
+                  alt="logo"
+                  width={80}
+                  height={20}
+                  className="w-full dark:hidden"
+                />
+                <Image
+                  src="/images/logo/dark.svg"
+                  alt="logo"
+                  width={100}
+                  height={20}
+                  className="hidden w-full dark:block"
+                />
+
+
+                {/**  <span
                   className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight"
                 >
                   Kanteen
-                </span>
+                </span>*/}
               </Link>
             </div>
             <div className="flex w-full items-center justify-between px-4">
@@ -85,8 +103,8 @@ const Header = () => {
                 <nav
                   id="navbarCollapse"
                   className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${navbarOpen
-                      ? "visibility top-full opacity-100"
-                      : "invisible top-[120%] opacity-0"
+                    ? "visibility top-full opacity-100"
+                    : "invisible top-[120%] opacity-0"
                     }`}
                 >
                   <ul className="block lg:flex lg:space-x-12">
@@ -96,8 +114,8 @@ const Header = () => {
                           <Link
                             href={menuItem.path}
                             className={`flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${usePathName === menuItem.path
-                                ? "text-green dark:text-white"
-                                : "text-dark hover:text-green/80 dark:text-white/70 dark:hover:text-white"
+                              ? "text-green dark:text-white"
+                              : "text-dark hover:text-green/80 dark:text-white/70 dark:hover:text-white"
                               }`}
                           >
                             {menuItem.title}
@@ -143,7 +161,7 @@ const Header = () => {
               </div>
               <div className="flex items-center justify-end pr-16 lg:pr-0">
                 <Link
-                  href="https://calendly.com/kanteenteam" 
+                  href="https://calendly.com/kanteenteam"
                   className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-md bg-primary px-8 py-3 text-base font-medium text-black dark:text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
                 >
                   Book a free session
