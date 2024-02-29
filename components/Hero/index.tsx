afe763890249584663d7ebef1df6876a9085a198
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-02-29 16:40:34 +0100

    commit

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -30,32 +30,73 @@ const Hero = () => {
 
 
                 <div className="-mx-4 flex flex-wrap">
-                  <div className="w-full px-4">
+
+
+                <div className="w-1/2 sm:w-1/4 px-1 py-1">
                     <div
                       className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
                       data-wow-delay=".15s"
                     >
-                      <div className="relative aspect-[77/40] items-center justify-center">
-                        <Image src="/images/blog/food.jpg" alt="video image" fill />
-                        <div className="absolute right-0 top-0 flex h-full w-full items-center justify-center">
-                          <button
-                            aria-label="video play button"
-                            onClick={() => setOpen(true)}
-                            className="flex h-[70px] w-[70px] items-center justify-center rounded-full bg-white bg-opacity-75 text-black transition hover:bg-opacity-100"
-                          >
-                            <svg
-                              width="16"
-                              height="18"
-                              viewBox="0 0 16 18"
-                              className="fill-current"
-                            >
-                              <path d="M15.5 8.13397C16.1667 8.51888 16.1667 9.48112 15.5 9.86602L2 17.6603C1.33333 18.0452 0.499999 17.564 0.499999 16.7942L0.5 1.20577C0.5 0.43597 1.33333 -0.0451549 2 0.339745L15.5 8.13397Z" />
-                            </svg>
-                          </button>
-                        </div>
+                      <div className="relative  items-center justify-center h-[400px]">
+                      <video autoPlay muted loop controls className="absolute inset-0 w-full h-full object-cover"  >
+                          <source src="/images/video/Nue.mp4" type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
                       </div>
                     </div>
                   </div>
+
+
+                  <div className="w-1/2 sm:w-1/4 px-1 py-1">
+                    <div
+                      className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
+                      data-wow-delay=".15s"
+                    >
+                      <div className="relative items-center justify-center h-[400px] ">
+                      <video controls autoPlay muted loop  className="absolute inset-0 w-full h-full object-cover">
+                          <source src="/images/video/Canlis.mp4" type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
+                       
+                      </div>
+                    </div>
+                  </div>
+
+
+
+                  <div className="w-1/2 sm:w-1/4 px-1 py-1">
+                    <div
+                      className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
+                      data-wow-delay=".15s"
+                    >
+                      <div className="relative  items-center justify-center h-[400px]">
+                        <video autoPlay muted loop  controls className="absolute inset-0 w-full h-full object-cover">
+                          <source src="/images/video/Tavolata.mp4" type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
+                      
+                      </div>
+                    </div>
+                  </div>
+
+
+
+
+                  <div className="w-1/2 sm:w-1/4 px-1 py-1">
+                    <div
+                      className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
+                      data-wow-delay=".15s"
+                    >
+                      <div className="relative items-center justify-center h-[400px]">
+                      <video autoPlay muted loop controls className="absolute inset-0 w-full h-full object-cover">
+                          <source src="/images/video/Nue.mp4" type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
+                       
+                      </div>
+                    </div>
+                  </div>
+
                 </div>
               </div>
 
@@ -70,7 +111,7 @@ const Hero = () => {
 
               <div className="flex flex-col pt-10 items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                 <Link
-                 href="#contact"
+                  href="#contact"
                   className="rounded-md bg-primary px-8 py-4 text-base font-semibold text-black dark:text-white duration-300 ease-in-out hover:bg-primary/80"
                 >
                   Join our whaitlist
