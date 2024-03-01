7e4585aae5d3f8bffd956773acb43eadeaa9ac95
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-03-01 12:16:08 +0100

    commit

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -41,10 +41,11 @@ const Header = () => {
   return (
     <>
       <header
-        className={`header left-0 top-0 z-40 flex w-full items-center ${sticky
-          ? "dark:bg-green dark:shadow-sticky-dark fixed z-[9999] bg-green !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
-          : "bg-green shadow-sticky-dark fixed z-[9999] !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
-          }`}
+        className={`header left-0 top-0 z-40 flex w-full items-center ${
+          sticky
+            ? "dark:bg-gray-dark dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
+            : "absolute bg-transparent shadow-sticky-dark"
+        }`}
       >
         <div className="container">
           <div className="relative -mx-4 flex items-center justify-between">
