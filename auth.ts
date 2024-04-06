855739a8065ba529aead1fcb551afb15adc66ca4
Sarahjedlaoui <sarajedlaoui999@gmail.com>
2024-04-06 15:12:37 +0200

    commit

---
+++ auth.ts
@@ -0,0 +1,24 @@
+ /**import NextAuth from "next-auth"
+import Cognito from "next-auth/providers/cognito"
+
+export const {handlers: {GET, POST}, auth, signIn, signOut} = NextAuth({
+    providers: [Cognito({
+        checks: ["state"],
+        clientId: "",
+        clientSecret: ""
+    })],
+
+    basePath: "/auth",
+    trustHost: true,
+    callbacks: {
+        authorized({request, auth}) {
+            // middleware auth filter
+
+            const {pathname} = request.nextUrl
+            if (pathname === "/test") return !!auth
+            return true
+        }
+    }, 
+    debug: process.env.NODE_ENV !== "production"
+});
+*/
