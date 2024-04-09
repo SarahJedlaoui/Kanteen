42d97952a575ffb5d8346f74692e23f9b9518f10
maherkhelifi88 <121508808+maherkhelifi88@users.noreply.github.com>
2024-04-09 15:57:36 +0200

    Update page.tsx

--- app/tokyohotfriedchicken/page.tsx
+++ app/tokyohotfriedchicken/page.tsx
@@ -1,6 +1,6 @@
 
 import Persona from "@/components/Persona";
-import Restaurant from "@/components/Persona/buvette";
+import Restaurant from "@/components/Persona/tokyo";
 import ScrollUp from "@/components/Common/ScrollUp";
 import { Metadata } from "next";
 
