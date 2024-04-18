59a87ab0d1b5dc6bb9eb93766cd6fd269dc227db
maherkhelifi88 <121508808+maherkhelifi88@users.noreply.github.com>
2024-04-18 10:23:59 +0100

    Delete app/api/test directory

--- app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/auth"
-
-export const GET = auth((req) => {
-    if (req.auth) {
-        return Response.json({ data: "Test data!" })
-    }
-
-    return Response.json({ message: "Not authenticated" }, { status: 401 })
-});
