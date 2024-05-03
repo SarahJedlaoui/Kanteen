a063761fb6a7dd5aa8cc5681aaa6d038bf360320
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-05-03 15:00:05 +0100

    commit

---
+++ app/the-ballard-cut/page.tsx
@@ -0,0 +1,22 @@
+
+import Persona from "@/components/Persona";
+import Restaurant from "@/components/Persona/ballard";
+import ScrollUp from "@/components/Common/ScrollUp";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Restaurant Page",
+  description: "This is Restaurant Page ",
+  // other metadata
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <ScrollUp />
+      <Restaurant />
+    </>
+  );
+};
+
+export default AboutPage;
