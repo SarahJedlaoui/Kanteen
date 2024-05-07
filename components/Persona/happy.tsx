5f0d38639ef50e06a490a80593d62c47fe8064f7
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-05-07 17:14:08 +0100

    commit

--- components/Persona/happy.tsx
+++ components/Persona/happy.tsx
@@ -138,17 +138,12 @@ const Restaurant = () => {
           \n video4:rate ${rate4}, decision: ${lastClicked4}, feedback:  ${feedback4}
           \n video5:rate ${rate5}, decision: ${lastClicked5}, feedback:  ${feedback5}
           \n video6:rate ${rate6}, decision: ${lastClicked6}, feedback:  ${feedback6}
-          \n video7:rate ${rate7}, decision: ${lastClicked7}, feedback:  ${feedback7}
-          \nvideo8:rate ${rate8}, decision: ${lastClicked8}, feedback:  ${feedback8}
-          \n video9:rate ${rate9}, decision: ${lastClicked9}, feedback:  ${feedback9}
-          \n video10:rate ${rate10}, decision: ${lastClicked10}, feedback:  ${feedback10}
-          \n video11:rate ${rate11}, decision: ${lastClicked11}, feedback:  ${feedback11}
-          \n video12:rate ${rate12} , decision: ${lastClicked12}, feedback:  ${feedback12} \n 
+         
           Feedback: ${feedback}\n
           `,
         }
       })
-      console.log('Reviw Submitted:', rate1, rate3, feedback);
+      console.log('Reviw Submitted:', feedback);
       setOpenAlert(true);
     } catch (error) {
       console.error('Error while saving data:', error);
