c019751935d0fd651e6fc82b42660214d24afc0f
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-05-09 13:29:01 +0100

    commit

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import ModalVideo from "react-modal-video";
 import Image from "next/image";
 import React, { useEffect, useState } from 'react';
-import './styles.css'
+
 
 
 const Hero = () => {
@@ -26,55 +26,47 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 mt-10 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Introducing Kanteen, an AI-Powered video creator for your restaurant
+                  Introducing Kanteen, an AI-Powered video creator for your restaurant
                 </h1>
 
                 <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
-                Our AI content creator transforms videos from your camera roll into captivating stories for your restaurant&apos;s Social Media.
+                  Our AI content creator transforms videos from your camera roll into captivating stories for your restaurant&apos;s Social Media.
                 </p>
 
 
-                <div className="-mx-4 flex flex-wrap">
+                <div className="-mx-3 flex flex-wrap items-center justify-center mb-5" >
 
 
-                  <div className="w-1/2 sm:w-1/4 px-1 py-1">
+                  <div className="w-1/2 sm:w-1/4 px-1 py-1" >
                     <div
                       className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
                       data-wow-delay=".15s"
                     >
-                      <div className="relative  items-center justify-center h-[400px]">
-                        <iframe
-                          src="https://player.vimeo.com/video/920479248?badge=0&autopause=0&player_id=0&app_id=58479&loop=1&controls=0&muted=1&autoplay=1"
-
-                          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-                          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                          title="Hearth"
-                        >
-                        </iframe>
-
+                      <div className="mb-5 relative items-center justify-center h-[400px] ">
+                        <video autoPlay muted loop className="absolute inset-0 w-full h-full object-cover">
+                          <source src="https://d205gdf6tf9tly.cloudfront.net/19.mp4" type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
                       </div>
                     </div>
                   </div>
 
 
-                  <div className="w-1/2 sm:w-1/4 px-1 py-1">
+
+                  <div className="w-1/2 sm:w-1/4 px-1 py-1" >
                     <div
                       className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
                       data-wow-delay=".15s"
                     >
-                      <div className="relative items-center justify-center h-[400px] ">
-                        <iframe
-                          src="https://player.vimeo.com/video/920474921?badge=0&autopause=0&player_id=0&app_id=58479&loop=1&controls=0&muted=1&autoplay=1"
-
-                          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-                          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                          title="mortonssteak"
-                        >
-                        </iframe>
-
+                      <div className="mb-5 relative items-center justify-center h-[400px] ">
+                        <video  autoPlay muted loop className="absolute inset-0 w-full h-full object-cover">
+                          <source src="https://d205gdf6tf9tly.cloudfront.net/tokyo2.mp4" type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
                       </div>
                     </div>
                   </div>
+
 
 
 
@@ -83,41 +75,42 @@ const Hero = () => {
                       className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
                       data-wow-delay=".15s"
                     >
-                      <div className="relative  items-center justify-center h-[400px]">
-                        <iframe
-                          src="https://player.vimeo.com/video/920480495?badge=0&autopause=0&player_id=0&app_id=58479&loop=1&controls=0&muted=1&autoplay=1"
-
-                          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-                          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                          title="maydan"
-                        >
-                        </iframe>
+                      <div className="mb-5 relative  items-center justify-center h-[400px]">
+                        <video autoPlay muted loop controls className="absolute inset-0 w-full h-full object-cover">
+                          <source src="https://d205gdf6tf9tly.cloudfront.net/cacked5.mp4" type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
 
                       </div>
+
                     </div>
+
                   </div>
+
 
 
 
 
                   <div className="w-1/2 sm:w-1/4 px-1 py-1">
                     <div
-                      className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-4lg" 
+                      className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
                       data-wow-delay=".15s"
                     >
-                      <div className="relative items-center justify-center h-[400px]">
-                        <iframe
-                          src="https://player.vimeo.com/video/920487294?badge=0&autopause=0&player_id=0&app_id=58479&loop=1&controls=0&muted=1&autoplay=1"
-                          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-                          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                          title="mortonssteak"
-                        ></iframe>
+                      <div className="mb-5 relative items-center justify-center h-[400px]">
+                        <video autoPlay muted loop controls className="absolute inset-0 w-full h-full object-cover">
+                          <source src="https://d205gdf6tf9tly.cloudfront.net/15.mp4" type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
+
                       </div>
+
                     </div>
                   </div>
-
 
                 </div>
+
+
+
               </div>
 
               <ModalVideo
